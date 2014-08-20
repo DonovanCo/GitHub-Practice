cbda0afd1d16251970f7bbfc359b9f8724cc127d
DonovanCo <donovanco@mullenmail.com>
2014-08-20 17:40:55 -0600

    added an output message and for loop

--- GitHub Test/src/GitHubTest.java
+++ GitHub Test/src/GitHubTest.java
@@ -3,18 +3,14 @@ public class GitHubTest
 	{
 	public static void main(String[] args)
 		{
-
+		System.out.println("I downloaded this at school.");
 		
-		System.out.println("Hello Mr.McGuire.");
-
-		for(int gitTest = 1; gitTest<=200; gitTest++)
+		System.out.println("Hello Mr. McGuire.");
+		
+		for(int i = 0; i<=50; i++)
 		{
-			if(gitTest%8==0)
-			{
-				System.out.println(gitTest);
-			}
+			System.out.println(i);
 		}
-		
 		//Open it up back at school to demonstrate you can move
 		//code back and forth and modify it both places.
 
