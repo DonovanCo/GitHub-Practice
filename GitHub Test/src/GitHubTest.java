a40453eb47d8b417a94226c2befea81dc58aa026
DonovanCo <donovanco@mullenmail.com>
2014-08-20 17:36:41 -0600

    Added a line of outout and a for loop that printed out numbers divisible by 8 from 1 - 200.

--- GitHub Test/src/GitHubTest.java
+++ GitHub Test/src/GitHubTest.java
@@ -3,13 +3,17 @@ public class GitHubTest
 	{
 	public static void main(String[] args)
 		{
-		System.out.println("I downloaded this at school.");
-		
-		//Add a line about modifying it at school, then upload it
-		// back to GitHub.
 
-		//Open this at home and add a line about modifying it there.
-		//Update it on GitHub.
+		
+		System.out.println("Hello Mr.McGuire.");
+
+		for(int gitTest = 1; gitTest<=200; gitTest++)
+		{
+			if(gitTest%8==0)
+			{
+				System.out.println(gitTest);
+			}
+		}
 		
 		//Open it up back at school to demonstrate you can move
 		//code back and forth and modify it both places.
